9f28a1dff950156fffb18b59c72e1a8396e3ad97
hosam <hosam.mohammed@scania.com>
2021-07-03 23:50:23 +0200

    Submitted assignment 3

---
+++ ass3_1.py
@@ -0,0 +1,8 @@
+hrs = float(input('Enter hours:'))
+rate = float(input('Enter rate:'))
+if hrs <= 40:
+    pay = hrs * rate
+else:
+    pay = 40 * rate
+    pay += (hrs - 40) * (rate * 1.5)
+print(pay)
