ec0e31e996d95d69ca20d53fb8e3362285368d04
hosamhamed <hosam.taha68@gmail.com>
2021-07-04 16:45:32 +0200

    change dictionary name

--- pythonNinja/dict.py
+++ pythonNinja/dict.py
@@ -3,16 +3,16 @@ def ninja_intro(dictionary):
     for key, value in dictionary.items():
         print(f'I am {key} and I am {value} belt')
 
-dict = {}
+ninja_dict = {}
 
 while True:
     name = input('Enter name: ')
     color = input('Enter belt color: ')
-    dict[name] = color
+    ninja_dict[name] = color
     another = input('add another? (y/n)')
     if another == 'y':
         continue
     else:
         break
 
-ninja_intro(dict)
+ninja_intro(ninja_dict)
