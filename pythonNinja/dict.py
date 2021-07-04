f9e6fbfdc5fc32fae41756152ebc0f2d8691f852
hosam <hosam.h.taha@gmail.com>
2021-07-04 16:27:58 +0200

    add function and dict simple examples

---
+++ pythonNinja/dict.py
@@ -0,0 +1,17 @@
+def ninja_intro(dictionary):
+    for key, value in dictionary.items():
+        print(f'I am {key} and I am {value} belt')
+
+dict = {}
+
+while True:
+    name = input('Enter name: ')
+    color = input('Enter belt color: ')
+    dict[name] = color
+    another = input('add another? (y/n)')
+    if another == 'y':
+        continue
+    else:
+        break
+
+ninja_intro(dict)
